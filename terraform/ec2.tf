994be8d913d213c80f25211afeba43182f17ebf5
Muskan Tandel <muskantandel824@gmail.com>
2026-02-07 21:18:45 +0530

    project started with git files

---
+++ terraform/ec2.tf
@@ -0,0 +1,29 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Deployed via Terraform + Jenkins</h1>" > /var/www/html/index.html
+              EOF
+
+  tags = {
+    Name = "terraform-web-server"
+  }
+}
---
+++ terraform/output.tf
@@ -0,0 +1,7 @@
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "website_url" {
+  value = "http://${aws_instance.web.public_ip}"
+}
