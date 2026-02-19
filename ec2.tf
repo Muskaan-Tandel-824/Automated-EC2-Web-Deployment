96e3b7c2c78f2e824fc05e40df71e8eafe99022b
Muskan Tandel <muskantandel824@gmail.com>
2026-02-19 13:07:55 +0530

    index.html updated

---
+++ ec2.tf
@@ -0,0 +1,33 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd git
+              systemctl start httpd
+              systemctl enable httpd
+
+              cd /var/www/html
+              rm -rf *
+              git clone https://github.com/Muskaan-Tandel-824/Automated-EC2-Web-Deployment.git .
+
+              EOF
+
+  tags = {
+    Name = "terraform-web-server"
+  }
+}
