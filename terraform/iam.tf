a0c05334596163e0c77031fe6cd507a9f34c79a5
Muskan Tandel <muskantandel824@gmail.com>
2026-04-18 09:09:25 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ec2_role" {
-  name = "terraform-ec2-profile-1"
+  name = "terraform-ec2-role-1"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -14,6 +14,6 @@ resource "aws_iam_role" "ec2_role" {
 }
 
 resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "terraform-ec2-profile"
+  name = "terraform-ec2-profile-1"
   role = aws_iam_role.ec2_role.name
 }
